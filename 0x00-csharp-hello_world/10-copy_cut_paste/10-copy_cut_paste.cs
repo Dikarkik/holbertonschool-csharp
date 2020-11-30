df55286e26d8a882e85d0265cc80b776492c51be
Diana Carolina Quintero <karodev3@gmail.com>
2020-11-30 17:32:42 -0500

    adjust task 10

--- 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 class Program
 {
     static void Main(string[] args)
@@ -8,8 +7,8 @@ class Program
         string word_first_3 = word.Substring(0, 3);
         string word_last_2 = word.Substring(word.Length - 2);
         string middle_word = word.Substring(1, word.Length - 2);
-        Console.WriteLine($"First 3 letters: {word_first_3}");
-        Console.WriteLine($"Last 2 letters: {word_last_2}");
-        Console.WriteLine($"Middle word: {middle_word}");
+        Console.WriteLine("First 3 letters: {0}", word_first_3);
+        Console.WriteLine("Last 2 letters: {0}", word_last_2);
+        Console.WriteLine("Middle word: {0}", middle_word);
     }
 }
