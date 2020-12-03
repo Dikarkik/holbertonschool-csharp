2790afebb4d14ac795071c29ae11192a6f3bfe45
Diana Carolina Quintero <karodev3@gmail.com>
2020-12-03 13:29:21 -0500

    task 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        if (myLList == null || myLList.First == null)
+            return 0;
+
+        LinkedListNode<int> current = myLList.First;
+        int sum = 0;
+
+        while (current != null)
+        {
+            sum += current.Value;
+            current = current.Next;
+        }
+
+        return sum;
+    }
+}
