c44367dcfceea27f5fb633059b731b2bb9b486f0
Diana Carolina Quintero <karodev3@gmail.com>
2020-12-03 13:11:38 -0500

    task 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        if (myLList == null)
+            return -1;
+
+        LinkedListNode<int> current = myLList.First;
+        int index = 0;
+
+        while (current != null)
+        {
+            if (current.Value != value)
+                index++;
+            else
+                return index;
+
+            current = current.Next;
+        }
+
+        return -1;
+    }
+}
