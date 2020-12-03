0ea1e4bfe7c39bb057233ab81f64812b67bb8044
Diana Carolina Quintero <karodev3@gmail.com>
2020-12-03 13:19:40 -0500

    Task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (myLList == null || myLList.Count == 0 || n < 0 || n > myLList.Count)
+            return 0;
+        
+        LinkedListNode<int> current = myLList.First;
+
+        for (int i = 0; i < n; i++)
+            current = current.Next;
+
+        return current.Value;
+    }
+}
