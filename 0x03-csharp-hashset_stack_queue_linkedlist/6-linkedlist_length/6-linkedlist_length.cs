aea707e8b07e92c9ec0d20a66db281e3b3db5206
Diana Carolina Quintero <karodev3@gmail.com>
2020-12-03 12:45:31 -0500

    Task 6 and 7

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        if (myLList == null)
+            return 0;
+
+        LinkedListNode<int> current = myLList.First;
+        int count = 0;
+
+        do
+        {
+            count++;
+            current = current.Next;
+        }
+        while (current != null);
+
+        return count;
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        if (myLList == null)
+            myLList = new LinkedList<int>();
+
+        return myLList.AddFirst(n);
+    }
+}
