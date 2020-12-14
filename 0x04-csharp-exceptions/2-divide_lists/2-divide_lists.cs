348aa1cbee4ff9df60f7013a988a972a06f8e7b8
Diana Carolina Quintero <karodev3@gmail.com>
2020-12-14 12:51:03 -0500

    Add tasks from 0 to 2

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> result = new List<int>();
+        int len = Math.Max(list1.Count, list2.Count);
+
+        for (int i = 0; i < len; i++)
+        {
+            try
+            {
+                result.Add(list1[i] / list2[i]);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                result.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+
+        return result;
+    }
+}
