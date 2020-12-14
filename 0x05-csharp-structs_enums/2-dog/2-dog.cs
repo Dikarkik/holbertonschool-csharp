c00b6cba87e4d4c5ad717dd1fcde8dacc56776d6
Diana Carolina Quintero <karodev3@gmail.com>
2020-12-14 14:11:19 -0500

    Tasks from 2 to 3

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,22 @@
+﻿public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name_, float age_, string owner_, Rating rating_)
+    {
+        name = name_;
+        age = age_;
+        owner = owner_;
+        rating = rating_;
+    }
+}
