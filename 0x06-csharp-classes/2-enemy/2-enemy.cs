6b99f78e7db6f00d1563cd7b25b2f655b602cb2c
Diana Carolina Quintero <karodev3@gmail.com>
2020-12-15 14:55:44 -0500

    task 2 and task 5

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Enemies
+{
+    /// <summary>
+    /// This class defines a zombie.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// zombie health
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Constructor using parameters
+        /// <param name="value">health</param>
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+
+            health = value;
+        }
+    }
+}
---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Enemies
+{
+    /// <summary>
+    /// This class defines a zombie
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// zombie health
+        /// </summary>
+        private int health;
+        private string name = "(No name)";
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Constructor using parameters
+        /// <param name="value">health</param>
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+
+            health = value;
+        }
+
+        /// <summary>
+        /// Property of 'name'
+        /// </summary>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        /// <summary>
+        /// Method that retrieves the value of 'health'
+        /// </summary>
+        public int GetHealth() => health;
+
+        /// <summary>
+        /// Retrieves the Zombie object’s attributes
+        /// </summary>
+        public override string ToString()
+        {
+            return $"Zombie name: {name} / Total Health: {health}";
+        }
+    }
+}
