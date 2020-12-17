c79abb53b9ee6bbee2bd5a6b0c537d2086ad8e1b
Diana Carolina Quintero <karodev3@gmail.com>
2020-12-17 13:53:46 -0500

    task 0: add comments, update tests

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -5,22 +5,12 @@ namespace MyMath.Tests
     [TestFixture]
     public class Tests
     {
-        [Test]
-        public void Test_Add_positive_numbers()
+        [TestCase(1, 2, 3)]
+        [TestCase(-1, -2, -3)]
+        [TestCase(3, -3, 0)]
+        public void Test_Add_positive_and_negative_numbers(int a, int b, int result)
         {
             Assert.AreEqual(Operations.Add(1, 2), 3);
-        }
-
-        [Test]
-        public void Test_Add_negative_numbers()
-        {
-            Assert.AreEqual(Operations.Add(-1, -2), -3);
-        }
-
-        [Test]
-        public void Test_Add_positive_and_negative_number()
-        {
-            Assert.AreEqual(Operations.Add(3, -3), 0);
         }
     }
 }
--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -2,8 +2,17 @@
 
 namespace MyMath
 {
+    /// <summary>
+    /// This class defines math operations
+    /// </summary>
     public class Operations
     {
+        /// <summary>
+        /// Method that adds 2 numbers
+        /// <param name="a">integer</param>
+        /// <param name="b">integer</param>
+        /// <returns>Sum of a and b</returns>
+        /// </summary>
         public static int Add(int a, int b)
         {
             return a + b;
