dc8edc07f3adae349dbb205143b60615abd06ab6
Diana Carolina Quintero <karodev3@gmail.com>
2020-12-19 14:50:18 -0500

    Add task 0: 0-is

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// This class defines operations for type checking
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// method that checks if an object is type int
+    /// <param name="obj">object to check</param>
+    /// <returns>True if 'obj' is an int, otherwise return False</returns>
+    /// </summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType() == typeof(int))
+            return true;
+
+        return false;
+    }
+}
