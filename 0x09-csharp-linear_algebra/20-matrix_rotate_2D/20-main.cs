741f7385000455ccc520cb6bf0fe2a7b342c5670
Diana Carolina Quintero <karodev3@gmail.com>
2021-01-18 16:00:30 -0500

    Add task 23

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-main.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-main.cs
@@ -10,6 +10,20 @@ class Program
 		matrix = new double[2, 2] { { 1, 2 }, { 3, 4 } };
 		res = MatrixMath.Rotate2D(matrix, -1.57); // (2, -1) (4, -3)
 		PrintMatrix(res);
+
+		// example 2
+		matrix = new double[2, 2] { { 1, 2 }, { 3, 4 } };
+		res = MatrixMath.Rotate2D(matrix, 3); // (-1.27, -1.84) (-3.53, -3.54)
+		PrintMatrix(res);
+
+		// example 3
+		matrix = new double[2, 2] { { 1.30, 2.12 }, { 3.98, 4.58 } };
+		res = MatrixMath.Rotate2D(matrix, 1.11); // (-1.34, 2.1) (-2.37, 5.6)
+		PrintMatrix(res);
+
+		matrix = new double[1, 1] { { 1 } };
+		res = MatrixMath.Rotate2D(matrix, 3); // (-1)
+		PrintMatrix(res);
 	}
 
 	static void PrintMatrix(double[,] matrix)
