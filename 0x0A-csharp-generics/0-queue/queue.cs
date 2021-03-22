c555dd8dacec63db4d36a2dfdedcb7469e171e28
Diana Carolina Quintero <karodev3@gmail.com>
2021-03-22 12:19:38 -0500

    Task 0. Queue

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Queue - Generic class
+/// </summary>
+/// <typeparam name="T">some type</typeparam>
+public class Queue<T>
+{
+	/// <summary>
+	/// CheckType - method to ckeck the Queue’s type
+	/// </summary>
+	/// <returns>the type of T</returns>
+	public string CheckType()
+	{
+		return typeof(T).ToString();
+	}
+}
