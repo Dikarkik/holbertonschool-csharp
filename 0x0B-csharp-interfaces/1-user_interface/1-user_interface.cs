dd5ebbefbfbe3ef130e1f5c4df86a918214e3c99
Diana Carolina Quintero <karodev3@gmail.com>
2021-03-23 12:57:00 -0500

    Task 1. User interface

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+/// <summary>
+/// Base - abstract class
+/// </summary>
+public abstract class Base
+{
+	/// <summary>
+	/// name - public property
+	/// </summary>
+	public string name;
+
+	/// <summary>
+	/// property for 'name'
+	/// </summary>
+	public string Name
+	{
+		get { return name; }
+		set { name = value; }
+	}
+
+	/// <summary>
+	/// ToString - override of 'ToString'
+	/// </summary>
+	/// <returns></returns>
+	public override string ToString()
+	{
+		return $"{name} is a {this.GetType()}";
+	}
+}
+
+interface IInteractive
+{
+	void Interact();
+}
+
+interface IBreakable
+{
+	int durability { get; set; }
+	void Break();
+}
+
+interface ICollectable
+{
+	bool isCollected { get; set; }
+	void Collect();
+}
+
+/// <summary>
+/// TestObject - class to test inherits from abstract class and interfaces
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+	/// <summary>
+	/// durability property
+	/// </summary>
+	public int durability { get; set; }
+
+	/// <summary>
+	/// isCollected property
+	/// </summary>
+	public bool isCollected { get; set; }
+
+	/// <summary>
+	/// Interact implementation
+	/// </summary>
+	public void Interact()
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Break implementation
+	/// </summary>
+	public void Break()
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Collect implementation
+	/// </summary>
+	public void Collect()
+	{
+		throw new NotImplementedException();
+	}
+}
