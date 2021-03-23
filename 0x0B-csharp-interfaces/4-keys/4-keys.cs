323d7c1d69c880159c657aaf12af85463b0a11c6
Diana Carolina Quintero <karodev3@gmail.com>
2021-03-23 14:34:03 -0500

    Task 4. Key collecting

---
+++ 0x0B-csharp-interfaces/4-keys/4-keys.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+/// <summary>
+/// Base - abstract class
+/// </summary>
+public abstract class Base
+{
+	/// <summary>
+	/// name - public property
+	/// </summary>
+	public string name { get; set; }
+
+	/// <summary>
+	/// ToString - override of 'ToString'
+	/// </summary>
+	/// <returns>'name' is a 'type'</returns>
+	public override string ToString()
+	{
+		return $"{name} is a {this.GetType()}";
+	}
+}
+
+interface IInteractive
+{
+	void Interact();
+}
+
+interface IBreakable
+{
+	int durability { get; set; }
+	void Break();
+}
+
+interface ICollectable
+{
+	bool isCollected { get; set; }
+	void Collect();
+}
+
+/// <summary>
+/// Decoration
+/// </summary>
+public class Key : Base, ICollectable
+{
+	/// <summary>
+	/// isCollected property
+	/// </summary>
+	public bool isCollected { get; set; }
+
+	/// <summary>
+	/// constructor
+	/// </summary>
+	/// <param name="name"></param>
+	/// <param name="isCollected"></param>
+	public Key(String name, bool isCollected)
+	{
+		this.name = name;
+		this.isCollected = isCollected;
+	}
+
+	/// <summary>
+	/// constructor
+	/// </summary>
+	/// <param name="isCollected"></param>
+	public Key(bool isCollected)
+	{
+		this.name = "Key";
+		this.isCollected = isCollected;
+	}
+
+	/// <summary>
+	/// constructor
+	/// </summary>
+	/// <param name="name"></param>
+	public Key(String name)
+	{
+		this.name = name;
+		this.isCollected = false;
+	}
+
+	/// <summary>
+	/// Collect implementation
+	/// </summary>
+	public void Collect()
+	{
+		if (!this.isCollected)
+		{
+			Console.WriteLine($"You pick up the {this.name}.");
+			this.isCollected = true;
+		}
+		else
+			Console.WriteLine($"You have already picked up the {this.name}.");
+	}
+}
