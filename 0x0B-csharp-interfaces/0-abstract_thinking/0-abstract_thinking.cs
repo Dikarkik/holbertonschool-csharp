ee2b0582a2eadb0983671867394a459c83b3c79b
Diana Carolina Quintero <karodev3@gmail.com>
2021-03-23 12:31:56 -0500

    Task 0. Abstract thinking

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/// <summary>
+/// Base - abstract class
+/// </summary>
+public abstract class Base
+{
+	/// <summary>
+	/// name - public property
+	/// </summary>
+	public string name;
+
+	/// <summary>
+	/// property for 'name'
+	/// </summary>
+	public string Name
+	{
+		get { return name; }
+		set { name = value; }
+	}
+
+	/// <summary>
+	/// ToString - override of 'ToString'
+	/// </summary>
+	/// <returns></returns>
+	public override string ToString()
+	{
+		return $"{name} is a {this.GetType()}";
+	}
+}
