a0b00fbf3d9f799b402acf4e937292ba26e2af37
Diana Carolina Quintero <karodev3@gmail.com>
2021-03-24 15:39:17 -0500

    Task 0. Universal health

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>
+/// class
+/// </summary>
+public class Player
+{
+	private String name { get; set; }
+	private float maxHp { get; set; }
+	private float hp { get; set; }
+
+	/// <summary>
+	/// constructor
+	/// </summary>
+	/// <param name="name"></param>
+	/// <param name="maxHp"></param>
+	public Player(String name = "Player", float maxHp = 100f)
+	{
+		this.name = name;
+
+		if (maxHp > 0)
+			this.maxHp = maxHp;
+		else
+		{
+			this.maxHp = 100f;
+			Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+		}
+
+		this.hp = this.maxHp;
+	}
+
+	/// <summary>
+	/// print the player's health
+	/// </summary>
+	public void PrintHealth()
+	{
+		Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+	}
+}
